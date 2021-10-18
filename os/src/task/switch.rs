17063ce3f02f3f023675384737f3d372d3ab9b14
buhe <bugu1986@126.com>
2021-10-18 12:12:12 +0800

    update docs

--- os/src/task/switch.rs
+++ os/src/task/switch.rs
@@ -1,4 +1,4 @@
-global_asm!(include_str!("switch.S"));
+global_asm!(include_str!("switch.asm"));
 
 extern "C" {
     pub fn __switch(current_task_cx_ptr2: *const usize, next_task_cx_ptr2: *const usize);
