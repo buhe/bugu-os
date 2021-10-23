9db8d550783796155a3f6d70c9bff75c9845c8f0
buhe <bugu1986@126.com>
2021-10-23 14:39:22 +0800

    Update fs.rs

--- os/src/scall_sbi/fs.rs
+++ os/src/scall_sbi/fs.rs
@@ -8,6 +8,7 @@ pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
             // 改为使用虚拟内存
             let buffers = translated_byte_buffer(current_user_token(), buf, len);
             for buffer in buffers {
+                // 使用宏
                 print!("{}", core::str::from_utf8(buffer).unwrap());
             }
             len as isize
