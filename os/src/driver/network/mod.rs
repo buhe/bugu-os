09eb54b1b5cd645f71f0d38b8b358b6ad61f05f8
buhe <bugu1986@126.com>
2021-11-11 17:38:46 +0800

    init uart1

--- os/src/driver/network/mod.rs
+++ os/src/driver/network/mod.rs
@@ -1,5 +1,39 @@
+use k210_hal::{prelude::*, serial::{Serial, Tx}};
+use k210_pac::{Peripherals, UART1};
+use self::{handler::SerialNetworkHandler, traits::Write};
+
 
 pub mod handler;
 pub mod response;
 pub mod traits;
 mod util;
+
+const DEFAULT_BAUD: u32 = 115_200;
+struct WA {
+    s: Tx<UART1>,
+}
+
+impl WA {
+    fn new(s: Tx<UART1>) -> Self {
+        Self {
+            s,
+        }
+    }
+}
+impl Write for WA{
+    type Error = ();
+    fn write_all(&mut self, buf: &[u8]) -> Result<(), Self::Error> {
+        for &b in buf {
+             self.s.try_write(b).unwrap();
+        }
+        Ok(())
+    }
+}
+pub fn init(){
+     let p = Peripherals::take().unwrap();
+     let clocks = k210_hal::clock::Clocks::new();
+     let uart1 = p.UART1.configure(DEFAULT_BAUD.bps(), &clocks);
+     let (tx, rx) = uart1.split();
+     let mut wa = WA::new(tx);
+     let h = SerialNetworkHandler::new(&mut wa, "" .as_bytes(), "".as_bytes());
+}
